f189388721ffdecaf4180c2731223049446e6049
derekling <derekling@Lings-Air.fritz.box>
2020-04-24 23:33:48 +0200

    Add animation

---
+++ timer.js
@@ -0,0 +1,47 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 10);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0){
+      this.onComplete();
+      this.pause();
+    } else {
+      this.onTick();
+      this.timeRemaining = this.timeRemaining - 0.01;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+
+}
